3a9f5a6f9c3b371695bf5cb33905fe7d64d90cd7
luizmosciaro <edu.mosciaro@live.com>
2023-03-18 12:02:12 -0400

    fix ssl 1.2

--- home/utils.py
+++ home/utils.py
@@ -4,9 +4,8 @@ from datetime import date
 from workalendar.america import Brazil
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from requests import adapters
+import urllib3
 import ssl
-from urllib3 import poolmanager
 
 def get_weather(city):
     api_key = os.getenv('WEATHER_API_KEY')
@@ -38,17 +37,23 @@ def get_selic():
     return {'selic':f'{(1 + float(interest)/100) ** workdays - 1:.2%}'}
 
 def get_ipca():
-    class TLSAdapter(adapters.HTTPAdapter):
+    class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+        # "Transport adapter" that allows us to use custom ssl_context.
+        def __init__(self, ssl_context=None, **kwargs):
+            self.ssl_context = ssl_context
+            super().__init__(**kwargs)
+
         def init_poolmanager(self, connections, maxsize, block=False):
-            """Create and initialize the urllib3 PoolManager."""
-            ctx = ssl.create_default_context()
-            ctx.set_ciphers('DEFAULT@SECLEVEL=1')
-            self.poolmanager = poolmanager.PoolManager(
-                    num_pools=connections,
-                    maxsize=maxsize,
-                    block=block,
-                    ssl_version=ssl.PROTOCOL_TLS,
-                    ssl_context=ctx)
+            self.poolmanager = urllib3.poolmanager.PoolManager(
+                num_pools=connections, maxsize=maxsize,
+                block=block, ssl_context=self.ssl_context)
+    
+    def get_legacy_session():
+        ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+        session = requests.session()
+        session.mount('https://', CustomHttpAdapter(ctx))
+        return session
 
     date_str = datetime.today()
     date_str = date_str - relativedelta(months=1)
@@ -57,12 +62,9 @@ def get_ipca():
     else:
         dt = f'{date_str.year}{date_str.month}'
 
-    url = f'https://servicodados.ibge.gov.br/api/v3/agregados/7060/periodos/{dt}/variaveis/63|69|2265?localidades=N1[all]'
-    
-    session = requests.session()
-    session.mount('https://', TLSAdapter())
-    response = session.get(url)
 
+    url = f'https://servicodados.ibge.gov.br/api/v3/agregados/7060/periodos/{dt}/variaveis/63|69|2265?localidades=N1[all]'
+    response = get_legacy_session().get(url)
     dataJson = response.json()
     context = {
         'montly_inflation' : dataJson[0]['resultados'][0]['series'][0]['serie'][dt],
