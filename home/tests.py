fac8f5923261ad4bf5d20fb39aa444439afdee87
luizmosciaro <edu.mosciaro@live.com>
2023-03-18 11:38:05 -0400

    Adjusted ssl certifi

--- home/tests.py
+++ home/tests.py
@@ -1,14 +1,1 @@
 from django.test import TestCase
-import requests
-from datetime import date
-from workalendar.america import Brazil
-
-start = date(2023,1,1)
-end = date(2023,12,31)
-cal = Brazil()
-workdays = cal.get_working_days_delta(start,end)
-
-taxa_selic_diaria = 0.62/100  # taxa diária atual (17/03/2023)
-taxa_selic_anual = (1 + taxa_selic_diaria) ** 12 - 1
-print(f"Taxa Selic anual: {taxa_selic_anual:.2%}")
-
