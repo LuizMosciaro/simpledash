0e13a35e822519dcea68620473c626a694f42d7a
luizmosciaro <edu.mosciaro@live.com>
2023-03-28 15:18:55 -0400

    Refactor tests and html

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,14 +1,17 @@
 from django.test import TestCase,Client
 from django.urls import reverse
 
-class TestViews(TestCase):
+class TestHomeView(TestCase):
     
     def setUp(self):
         self.client = Client()
         self.home_url = reverse('home')
         
 
-    def test_home_get(self):
+    def test_home_get_returns_correct_html(self):
         response = self.client.get(self.home_url)
-        self.assertEquals(response.status_code,200)
+        self.assertEqual(response.status_code,200)
+        self.assertIn('<title>SimpleDash</title>',response.content.decode())
+        self.assertTrue(response.content.decode().endswith('</html>'))
         self.assertTemplateUsed(response,'home/index.html')
+    
